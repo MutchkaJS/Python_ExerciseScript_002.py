89c956f2a532ce67269efffff4edd047c5b6dca2
John S. Mutchka <132361004+MutchkaJS@users.noreply.github.com>
2026-01-11 21:47:03 -0500

    Add script to repo for problem 2

---
+++ Python_ExerciseScript_002.py
@@ -0,0 +1,20 @@
+'''
+Python Exercise 002
+
+Write Python code to iterate through the first 10 numbers and, in each iteration,
+print the sum of the current and previous number.
+
+This will be done using a range() function and
+a calculating a simple sum.
+'''
+
+# Generate a list of ten numbers.
+
+previous = 0  # Initializes the var 'previous' = 0 to hold place values
+print("Printing current and previous number sum up to 10th number:")
+
+for i in range(10): # begin creating a series of 10 numbers
+    current_sum = i + previous  # create current_sum
+    print(f"Current Number {i}, Previous Number {previous}, Sum: {current_sum}")
+    previous = i
+
